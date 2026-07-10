22b2db9bb215c1ce1ec6d43a58a48f346c0fb582
LAB02-PC11 <LAB02-PC11@DESKTOP-9HN1T5T>
2026-07-10 09:00:59 -0500

    Encriptar

---
+++ Semana06/EncriptarClave/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncriptarClave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncriptar_Click(object sender, RoutedEventArgs e)
+        {
+            string clave = txtClave.Text;
+
+            if (string.IsNullOrWhiteSpace(clave))
+            {
+                MessageBox.Show("Ingrese una clave.");
+                return;
+            }
+
+            StringBuilder encriptada = new StringBuilder();
+
+            foreach (char c in clave)
+            {
+                encriptada.Append((char)(c + 3));
+            }
+
+            txtResultado.Text = encriptada.ToString();
+        }
+
+        private void btnDesencriptar_Click(object sender, RoutedEventArgs e)
+        {
+            string clave = txtResultado.Text;
+
+            if (string.IsNullOrWhiteSpace(clave))
+            {
+                MessageBox.Show("No existe una clave encriptada.");
+                return;
+            }
+
+            StringBuilder desencriptada = new StringBuilder();
+
+            foreach (char c in clave)
+            {
+                desencriptada.Append((char)(c - 3));
+            }
+
+            txtResultado.Text = desencriptada.ToString();
+        }
+    }
+}
