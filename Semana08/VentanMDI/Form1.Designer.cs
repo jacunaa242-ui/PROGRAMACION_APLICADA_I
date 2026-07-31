f7f74eaa1a155286c862db203fefbeece5709ba8
LAB02_PC15 <LAB02_PC15@DESKTOP-SCJ0B5O>
2026-07-31 08:54:35 -0500

    Nuevos Proyectos

---
+++ Semana08/VentanMDI/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace VentanMDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            mÓDULOToolStripMenuItem = new ToolStripMenuItem();
+            clienteToolStripMenuItem = new ToolStripMenuItem();
+            proveedorToolStripMenuItem = new ToolStripMenuItem();
+            vENTANAToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mÓDULOToolStripMenuItem, vENTANAToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // mÓDULOToolStripMenuItem
+            // 
+            mÓDULOToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { clienteToolStripMenuItem, proveedorToolStripMenuItem });
+            mÓDULOToolStripMenuItem.Name = "mÓDULOToolStripMenuItem";
+            mÓDULOToolStripMenuItem.Size = new Size(70, 20);
+            mÓDULOToolStripMenuItem.Text = "MÓDULO";
+            // 
+            // clienteToolStripMenuItem
+            // 
+            clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            clienteToolStripMenuItem.Size = new Size(180, 22);
+            clienteToolStripMenuItem.Text = "Cliente";
+            // 
+            // proveedorToolStripMenuItem
+            // 
+            proveedorToolStripMenuItem.Name = "proveedorToolStripMenuItem";
+            proveedorToolStripMenuItem.Size = new Size(180, 22);
+            proveedorToolStripMenuItem.Text = "Proveedor";
+            // 
+            // vENTANAToolStripMenuItem
+            // 
+            vENTANAToolStripMenuItem.Name = "vENTANAToolStripMenuItem";
+            vENTANAToolStripMenuItem.Size = new Size(72, 20);
+            vENTANAToolStripMenuItem.Text = "VENTANA";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mÓDULOToolStripMenuItem;
+        private ToolStripMenuItem clienteToolStripMenuItem;
+        private ToolStripMenuItem proveedorToolStripMenuItem;
+        private ToolStripMenuItem vENTANAToolStripMenuItem;
+    }
+}
---
+++ Semana08/VentanMDI/Form1.cs
@@ -0,0 +1,10 @@
+namespace VentanMDI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
