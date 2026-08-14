e139a50c1d7e218660dcd612a9378b600964d634
LAB02-PC11 <LAB02-PC11@DESKTOP-9HN1T5T>
2026-08-14 08:59:15 -0500

    semana 11

---
+++ Semana11/EjemploMVVM/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EjemploMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string cn = "Server=.;Database=Northwind;Integrated Security=True;TrustServerCertificate=True;Encrypt=True";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = " SELECT ProductID,ProductName, UnitPrice, Discontinued FROM Products";
+            using (SqlConnection conex= new SqlConnection(cn))
+            {
+                SqlDataAdapter da = new SqlDataAdapter( query,conex);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+                DgProductos.ItemsSource = dt.DefaultView;
+            }
+        }
+    }
+}
---
+++ Semana11/EjemploMVVM/Model/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploMVVM.Model
+{
+    internal class Producto
+    {
+        public int Id { get; set; }
+
+        public string nombre { get; set; }
+
+        public decimal precio { get; set; }
+
+        public bool discontinuado { get; set; }
+
+    }
+}
