aa21105266ffcaa664748e95ef12644c8db23be5
emerson940 <jhoncaluateran190@gmail.com>
2026-08-31 09:43:45 -0500

    Ventana Producto

---
+++ Semana13/ActualizacionRegistros/Producto.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ActualizacionRegistros
+{
+    /// <summary>
+    /// Lógica de interacción para Producto.xaml
+    /// </summary>
+    public partial class Producto : Window
+    {
+        public Producto()
+        {
+            InitializeComponent();
+        }
+
+        private void dgCategorias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            txtId.Clear();
+            txtNombre.Clear();
+           
+        }
+    }
+}
