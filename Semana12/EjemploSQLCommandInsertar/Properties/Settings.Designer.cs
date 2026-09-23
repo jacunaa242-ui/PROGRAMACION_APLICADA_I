62292770ad04c609b188f8d296c7c1161f4a6734
Carlos Acuña <jacunaa_24_2@unc.edu.pe>
2026-09-22 23:57:56 -0500

    cambio semana 12

--- Semana12/EjemploSQLCommandInsertar/Properties/Settings.Designer.cs
+++ Semana12/EjemploSQLCommandInsertar/Properties/Settings.Designer.cs
@@ -8,11 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace EjemploSQLComandInsertar.Properties {
+namespace EjemploSQLCommandInsertar.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "18.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,8 @@ namespace EjemploSQLComandInsertar.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=.;Database=Northwind;Integrated Security=True;TrustServerCertificate=True;" +
-            "Encrypt=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=localhost;Initial Catalog=Northwind;Integrated Security=True;TrustSer" +
+            "verCertificate=True")]
         public string Northwind {
             get {
                 return ((string)(this["Northwind"]));
